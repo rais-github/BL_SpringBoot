4615347bf6fde51d185b4ee179208a709924be71
Himanshu Rai <himanshu2003rai@gmail.com>
2025-03-01 14:23:40 +0530

    hello message from sts

--- src/main/java/com/backend/BL_task1_springboot/BlTask1SpringbootApplication.java
+++ src/main/java/com/backend/BL_task1_springboot/BlTask1SpringbootApplication.java
@@ -2,6 +2,8 @@ package com.backend.BL_task1_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BlTask1SpringbootApplication {
@@ -9,5 +11,13 @@ public class BlTask1SpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlTask1SpringbootApplication.class, args);
 	}
-		
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
