64d3894a55d865bdf4f3b9f256f2fff81fd5bb28
Himanshu Rai <himanshu2003rai@gmail.com>
2025-03-01 14:13:59 +0530

    Spring Initilizer Hello

---
+++ src/main/java/com/backend/BL_task1_springboot/BlTask1SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.backend.BL_task1_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlTask1SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlTask1SpringbootApplication.class, args);
+	}
+		
+}
---
+++ src/test/java/com/backend/BL_task1_springboot/BlTask1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.BL_task1_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlTask1SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
